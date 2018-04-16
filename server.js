ea1d83fda02e2ea16ce517a5c5221438fc420475
Ka Ching Co <kachingco@Kade-MacBook.local>
2018-04-16 16:32:30 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs'); //value set, value view
+
+
+app.use((req, res, next) => { //register a express, next means async
+    var now = new Date().toString(); //human readable timestamp
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log); //see what website they are going and time
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.')
+        }
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public')); //built in piece middleware, its like app.get()
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+}) //name, function
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+app.get('/', (req, res) => { //req: request, res: response, localhost:3000
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+    });
+});
+
+app.get('/about', (req, res) => { //localhost:3000/about
+    res.render('about.hbs', { //inside {} is object and dynamically reference the about.hbs
+        pageTitle: 'About Page',
+    }); //to render to our view or hbs we have now
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+}); //port
